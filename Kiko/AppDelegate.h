0b6dd47f564612372078d609f218159c19023096
Garrett Davidson <gdavids@purdue.edu>
2015-12-07 11:35:12 -0500

    Initial Commit

---
+++ Kiko/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kiko
+//
+//  Created by Garrett Davidson on 12/7/15.
+//  Copyright © 2015 G&R. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
